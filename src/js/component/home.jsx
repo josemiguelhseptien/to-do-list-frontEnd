61c98a50b253b920d81cfdb0af9543204044bcb5
josemiguelhseptien <josemiguelhseptien@gmail.com>
2022-04-06 00:43:53 +0000

    saved to continue

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [taskArray, setTaskArray] = useState([]);
+
+	let removeLi = setTaskArray.filter((task) => {
+		return "";
+	});
+
+	let mappedTaskArray = taskArray.map((task) => {
+		return (
+			<li className="list-group-item d-flex justify-content-between">
+				{task}
+				<button type="button" class="btn btn-outline-light">
+					Light
+				</button>
+			</li>
+		);
+	});
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="mainBox">
+				<div className="inputDiv">
+					<input
+						className="form-control"
+						id="newTask"
+						value={inputValue}
+						onChange={(e) => {
+							setInputValue(e.target.value);
+						}}
+						onKeyUp={(e) => {
+							if (e.key === "Enter") {
+								setTaskArray([...taskArray, inputValue]);
+								setInputValue("");
+							}
+						}}
+						placeholder="blank"
+					/>
+					<ul className="list-group">{mappedTaskArray}</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
